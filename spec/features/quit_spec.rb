e8132334155e387bb61ef4b977ed7f27a848f584
haazime <h4zime@gmail.com>
2016-05-28 17:53:07 +0900

    quit only

---
+++ spec/features/quit_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe 'Quit app' do
+  it do
+    input = InputStub.new(["0\n"])
+    output = OutputSpy.new
+
+    app = FizzBuzzer::App.new
+    cli = FizzBuzzer::CLI.new(app, input, output)
+    cli.start
+
+    outputs = output.buffers
+    expect(outputs[0]).to eq(
+      <<-EOU
+メニューを選択してください
+1: FizzBuzzをスタート
+0: 終了
+      EOU
+    )
+  end
+end
