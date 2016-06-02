45de376e57552e9ee6285ee122ac634c93bd9ef8
haazime <h4zime@gmail.com>
2016-06-02 21:57:28 +0900

    update metrics

--- tmp/metric_fu/output/cane.js
+++ tmp/metric_fu/output/cane.js
@@ -1,3 +1,3 @@
         var graph_title = 'Cane: code quality threshold violations';
-        var graph_series = [{name: 'cane', data: [16]}];
+        var graph_series = [{name: 'cane', data: [17]}];
         var graph_labels = {"0":"6/2"};
--- tmp/metric_fu/output/reek.js
+++ tmp/metric_fu/output/reek.js
@@ -1,3 +1,3 @@
         var graph_title = 'Reek: code smells';
-        var graph_series = [{name: 'IrresponsibleModule', data: [12]},{name: 'UnusedParameters', data: [2]},{name: 'UtilityFunction', data: [2]},{name: 'FeatureEnvy', data: [1]}];
+        var graph_series = [{name: 'IrresponsibleModule', data: [13]},{name: 'UtilityFunction', data: [5]},{name: 'UnusedParameters', data: [2]},{name: 'FeatureEnvy', data: [1]}];
         var graph_labels = {"0":"6/2"};
